9fb712c043f8c4131ebdf40dfd7e735cc299bf65
tim <timshen91@gmail.com>
2014-01-13 22:37:29 -0500

    procedurize

--- a.s
+++ a.s
@@ -1,16 +1,32 @@
 .code16
 .global _start
 _start:
-	movb $0x0e, %ah
-	movl $str, %ebx
-loop:
-	movb (%ebx), %al
-	cmpb $0, %al
-	jz idle
-	int $0x10
-	inc %ebx
-	jmp loop
+	mov $str, %edi
+	call puts
 idle:
 	jmp idle
+
+puts:
+	push %eax
+	mov %edi, %eax
+	puts_1b:
+		mov (%eax), %dl
+		cmp $0, %dl
+		jz puts_1f
+		call putchar
+		inc %eax
+		jmp puts_1b
+	puts_1f:
+	pop %eax
+	ret
+
+putchar:
+	push %ax
+	mov $0x0e, %ah
+	mov %dl, %al
+	int $0x10
+	pop %ax
+	ret
+
 str:
 	.string "hello, world\r\n"
